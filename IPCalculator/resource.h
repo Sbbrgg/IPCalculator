937b60cf9df57eb99f58c643678bf1ee6128cbfe
un <un@DESKTOP-JAU235M>
2026-05-20 20:19:15 +0300

    DialogBox DONE

--- IPCalculator/resource.h
+++ IPCalculator/resource.h
@@ -2,12 +2,14 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDD_DIALOG_MAIN                 101
 #define IDC_STATIC_IP_MASK              1001
 #define IDC_STATIC_IP_ADDRESS           1002
+#define IDC_STATIC_INFO                 1002
+#define IDC_STATIC_FRAME                1002
 #define IDC_IP_ADDRESS                  1003
-#define IDC_IPADDRESS2                  1004
 #define IDC_IP_MASK                     1004
+#define IDC_EDIT_PREFIX                 1005
+#define IDC_SPIN_PREFIX                 1006
 
 // Next default values for new objects
 // 
@@ -15,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
