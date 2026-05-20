eec7db7d01644424f5c2bf1cc10ee04b045bbb73
un <un@DESKTOP-JAU235M>
2026-05-20 21:41:02 +0300

    IPmask detection DONE TODO: Prefix

--- IPCalculator/resource.h
+++ IPCalculator/resource.h
@@ -4,12 +4,13 @@
 //
 #define IDC_STATIC_IP_MASK              1001
 #define IDC_STATIC_IP_ADDRESS           1002
-#define IDC_STATIC_INFO                 1002
-#define IDC_STATIC_FRAME                1002
+#define IDC_DIALOG_MAIN                 1002
+#define IDD_DIALOG_MAIN                 1002
 #define IDC_IP_ADDRESS                  1003
 #define IDC_IP_MASK                     1004
 #define IDC_EDIT_PREFIX                 1005
 #define IDC_SPIN_PREFIX                 1006
+#define IDC_STATIC_INFO                 1008
 
 // Next default values for new objects
 // 
@@ -17,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
